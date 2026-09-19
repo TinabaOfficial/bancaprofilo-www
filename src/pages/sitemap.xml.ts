405fee059651e454985131168c06eb4af6e8c601
Alberto Santini <a.santini@tinaba.it>
2026-09-19 15:29:28 +0200

    style: format readable source artifacts

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -6,7 +6,13 @@ const siteUrl = 'https://tinaba.bancaprofilo.it';
 export const GET: APIRoute = () => {
   const paths = ['/', '/soluzioni/', '/brand-guidelines/', ...editorialPages.map((page) => `/${page.slug}/`)];
   const urls = [...new Set(paths)].map((path) => `  <url><loc>${siteUrl}${path}</loc></url>`).join('\n');
-  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}\n</urlset>\n`;
+  const body = [
+    '<?xml version="1.0" encoding="UTF-8"?>',
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    urls,
+    '</urlset>',
+    '',
+  ].join('\n');
 
   return new Response(body, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } });
 };
